97ad28a64327cf7cf53303c39b5b3c473eae55d4
Jonathan Bohren <jbo@jhu.edu>
2013-02-19 16:57:26 -0500

    about to remove this stuff

---
+++ include/composite_controllers/controller_interface.h
@@ -0,0 +1,13 @@
+#ifndef __COMPOSITE_CONTROLLERS_CONTROLLER_INTERFACE_H
+#define __COMPOSITE_CONTROLLERS_CONTROLLER_INTERFACE_H
+
+namespace composite_controllers {
+  template<class InputInterface, class HWInterface>
+    class CompositeLink 
+    {
+    public:
+    private:
+    };
+}
+
+#endif // ifndef __COMPOSITE_CONTROLLERS_CONTROLLER_INTERFACE_H
---
+++ include/composite_controllers/joint_command_sum_interface.h
@@ -0,0 +1,13 @@
+#ifndef COMPOSITE_CONTROLLERS_JOINT_SUM_COMMAND_INTERFACE_H
+#define COMPOSITE_CONTROLLERS_JOINT_SUM_COMMAND_INTERFACE_H
+
+namespace composite_controllers {
+  
+  class JointSumHandle : public JointSumHandle
+  {
+    
+  }
+
+}
+
+#endif // ifndef COMPOSITE_CONTROLLERS_JOINT_SUM_COMMAND_INTERFACE_H
